6925d403923c842eecddd3b147a7314e7cd8296b
Pradnya Ghuge <gpradnya99@gmail.com>
2024-03-15 04:01:37 +0530

    brand page

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo"></img>
+      </div>
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+      <button>LOGIN</button>
+    </nav>
+  );
+};
+export default Navigation;
